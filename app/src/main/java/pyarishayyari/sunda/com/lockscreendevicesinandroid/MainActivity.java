c6b46fdacc4f11291e33071c9a6f84acec838d92
Sunder <sunderrra31@gmail.com>
2018-09-13 16:04:07 +0530

    Initial commit

---
+++ app/src/main/java/pyarishayyari/sunda/com/lockscreendevicesinandroid/MainActivity.java
@@ -0,0 +1,80 @@
+package pyarishayyari.sunda.com.lockscreendevicesinandroid;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button lock, disable, enable;
+    public static final int RESULT_ENABLE = 11;
+    private DevicePolicyManager devicePolicyManager;
+    private ActivityManager activityManager;
+    private ComponentName compName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        compName = new ComponentName(this, MyAdmin.class);
+        lock = findViewById(R.id.lock);
+        enable = findViewById(R.id.enableBtn);
+        disable =findViewById(R.id.disableBtn);
+        lock.setOnClickListener(this);
+        enable.setOnClickListener(this);
+        disable.setOnClickListener(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean isActive = devicePolicyManager.isAdminActive(compName);
+        disable.setVisibility(isActive ? View.VISIBLE : View.GONE);
+        enable.setVisibility(isActive ? View.GONE : View.VISIBLE);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == lock) {
+            boolean active = devicePolicyManager.isAdminActive(compName);
+
+            if (active) {
+                devicePolicyManager.lockNow();
+            } else {
+                Toast.makeText(this, "You need to enable the Admin Device Features", Toast.LENGTH_SHORT).show();
+            }
+
+        } else if (v == enable) {
+
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Additional text explaining why we need this permission");
+            startActivityForResult(intent, RESULT_ENABLE);
+
+        } else if (v == disable) {
+            devicePolicyManager.removeActiveAdmin(compName);
+            disable.setVisibility(View.GONE);
+            enable.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case RESULT_ENABLE :
+                if (resultCode == Activity.RESULT_OK) {
+                    Toast.makeText(MainActivity.this, "You have enabled the Admin Device features", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Problem to enable the Admin Device features", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
